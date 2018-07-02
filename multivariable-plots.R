0bd02833945378172a60ca77c967e5869855b470
wparedesgt <wparedesgt@gmail.com>
2018-07-02 14:15:18 -0600

    Cambiando valores

--- multivariable-plots.R
+++ multivariable-plots.R
@@ -28,6 +28,16 @@ plot <- bike %>% ggplot(aes(temp,cnt))
 
 #generando el plot
 
-plot + geom_point(size=3, aes(color=factor(windspeed))) + 
-  geom_smooth(method="lm", se=FALSE, col="red") + 
-  facet_grid(weekday ~ season) + theme(legend.position="bottom")
+plot + geom_point(size=3, aes(color=factor(windspeed))) + geom_smooth(method="lm", se=FALSE, col="red") + facet_grid(weekday ~ season) + theme(legend.position="bottom")
+
+#Leyendo otra data
+
+auto <- read.csv("data/auto-mpg.csv", stringsAsFactors=FALSE)
+auto$cylinders <- factor(auto$cylinders,labels=c("3cyl","4cyl", "5cyl","6cyl","8cyl"))
+ggpairs(auto[,2:5])
+
+
+ggpairs(auto[,2:5], aes(colour = cylinders, alpha = 0.4 ), title = "Multivariate Analysis") + theme(plot.title = element_text(hjust = 0.5))
+
+ggpairs(auto[,2:5], aes(colour = cylinders, alpha = 0.4 ), title = "Multivariate Analysis", upper = list(continuous = "density"), lower = list(combo = "denstrip")) + theme(plot.title = element_text(hjust = 0.5))
+
