8abc4a8397b0fdbc75a47fc6e65cba141211b802
wparedesgt <wparedesgt@gmail.com>
2018-07-02 14:11:48 -0600

    Plots multivariables

--- multivariable-plots.R
+++ multivariable-plots.R
@@ -7,3 +7,27 @@ library(GGally)
 
 bike <- read.csv("data/daily-bike-rentals.csv")
 
+#Cambiando los nombres de las variables
+
+bike$season <- factor(bike$season, levels = c(1,2,3,4), 
+                      labels = c("Primavera", "Verano", "Otoño", "Invierno"))
+bike$weathersit <- factor(bike$weathersit, levels = c(1,2,3), 
+                          labels = c("Despejado", "Nublado", "Nieve Ligera"))
+bike$windspeed.fac <- cut(bike$windspeed, breaks=3, 
+                          labels=c("Bajo", "Medio", "Alto"))
+bike$weekday <- factor(bike$weekday, levels = c(0:6), 
+                       labels = c("Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"))
+#Cargando la data a memoria
+
+attach(bike)
+
+
+#Creando la variable a dibujar
+
+plot <- bike %>% ggplot(aes(temp,cnt))
+
+#generando el plot
+
+plot + geom_point(size=3, aes(color=factor(windspeed))) + 
+  geom_smooth(method="lm", se=FALSE, col="red") + 
+  facet_grid(weekday ~ season) + theme(legend.position="bottom")
